e5687a61361b6b2acee7c93114d560ce969b6af4
ghals5757 <ghals5737@naver.com>
2024-10-14 21:22:12 +0900

    merge main

--- src/main/java/org/example/booknotelogserver/booknotelogserver/activity/domain/ActivityDeserializer.java
+++ src/main/java/org/example/booknotelogserver/booknotelogserver/activity/domain/ActivityDeserializer.java
@@ -10,45 +10,39 @@ import org.example.booknotelogserver.booknotelogserver.activity.domain.target.Ta
 import org.example.booknotelogserver.booknotelogserver.activity.domain.target.book.BookTarget;
 import org.example.booknotelogserver.booknotelogserver.activity.domain.target.book.BookUpdateTarget;
 import org.example.booknotelogserver.booknotelogserver.activity.domain.target.memo.MemoTarget;
-import org.example.booknotelogserver.booknotelogserver.activity.domain.target.memo.MemoUpdateTarget;
 import org.example.booknotelogserver.booknotelogserver.activity.domain.target.user.UserTarget;
 import org.example.booknotelogserver.booknotelogserver.activity.domain.target.user.UserUpdateTarget;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
+
 
 public class ActivityDeserializer extends JsonDeserializer<Activity> {
 
     private final ObjectMapper objectMapper;
 
-    // ObjectMapper를 주입받도록 설정 (Spring이 관리하는 ObjectMapper)
     public ActivityDeserializer() {
         this.objectMapper = new ObjectMapper();
     }
 
     @Override
     public Activity deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        // 전체 JSON 트리
         JsonNode rootNode = p.getCodec().readTree(p);
 
-        // 각 필드 추출
+
         String id = rootNode.get("id").asText();
         String action = rootNode.get("action").asText();
         JsonNode actorNode = rootNode.get("actor");
         JsonNode targetNode = rootNode.get("target");
         String timestamp = rootNode.get("timestamp").asText();
 
-        // Actor는 간단히 처리 가능하다고 가정
         Actor actor = objectMapper.readValue(actorNode.toString(), Actor.class);
 
-        // Target 필드 처리: action에 따라 구체적인 타입으로 변환
         Target target = determineTargetType(action, targetNode);
 
         return new Activity(id, action, actor, target, timestamp);
     }
 
-    // action에 따라 구체적인 Target 클래스를 결정하는 로직
+
     private Target determineTargetType(String action, JsonNode targetNode) throws IOException {
         return switch (action) {
             case "book.select", "book.delete", "book.create", "memo.update" -> objectMapper.readValue(targetNode.toString(), new TypeReference<BookTarget>() {});
