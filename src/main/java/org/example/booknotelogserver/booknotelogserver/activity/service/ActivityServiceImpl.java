58da06d439ad4b0c968534330e2810bdb912b571
ghals5737 <ghals5737@naver.com>
2024-10-14 21:27:56 +0900

    merger

--- src/main/java/org/example/booknotelogserver/booknotelogserver/activity/service/ActivityServiceImpl.java
+++ src/main/java/org/example/booknotelogserver/booknotelogserver/activity/service/ActivityServiceImpl.java
@@ -24,12 +24,8 @@ public class ActivityServiceImpl implements ActivityService {
     @KafkaListener(topics = topic, groupId = "${spring.kafka.consumer.group-id}")
     public void consumeActivityLog(String activityJson) {
         try {
-            // JSON 문자열을 Activity 객체로 역직렬화
             Activity activity = objectMapper.readValue(activityJson, Activity.class);
-            // 로그 출력 및 MongoDB 저장 (예시)
             activityRepository.save(ActivityEntity.from(activity));
-            System.out.println("Consumed activity log: " + activity);
-            // 예시: activityRepository.save(activity);
         } catch (Exception e) {
             e.printStackTrace(); // 오류 처리
         }
